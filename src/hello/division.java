2f9b42128535b2b0b5732c6d6f4c27dace564afa
Vamsib <97467084+vamsib-298@users.noreply.github.com>
2022-01-12 19:36:33 +0530

    modified integers

--- src/hello/division.java
+++ src/hello/division.java
@@ -3,8 +3,8 @@ package hello;
 public class division {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println(10/3);
+	
+        System.out.println(20/4);
 	}
 
 }
